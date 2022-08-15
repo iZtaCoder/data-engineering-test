76ae86529d82188555edf8a036c8c4955bada2fc
iZtaCoder <yrazu.mtz@gmail.com>
2022-08-15 03:25:25 -0500

    Update db_engine.py

--- db_engine.py
+++ db_engine.py
@@ -1,13 +1,17 @@
 from sqlalchemy import create_engine
 
-class DBEngine:
 
+class DBEngine:
     def __init__(self) -> None:
         self.db_user = "postgres"
         self.db_pwd = "root"
         self.db_host = "localhost"
         self.db_port = "5432"
         self.db_name = "twitter"
-    
+        self.engine = None
+
     def get_engine(self):
-        return create_engine(f'postgresql://{self.db_user}:{self.db_pwd}@{self.db_host}:{self.db_port}/{self.db_name}')
+        self.engine = create_engine(
+            f"postgresql://{self.db_user}:{self.db_pwd}@{self.db_host}:{self.db_port}/{self.db_name}"
+        )
+        return self.engine
